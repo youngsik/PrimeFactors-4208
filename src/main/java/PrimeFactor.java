2588cbac5d7489e45dbd8c2bb4199423e98f2ebf
dean <dean4208@gmail.com>
2025-06-04 10:38:45 +0900

    [refactoring] 54p

--- src/main/java/PrimeFactor.java
+++ src/main/java/PrimeFactor.java
@@ -4,26 +4,12 @@ import java.util.List;
 public class PrimeFactor {
     public Object of(int number) {
         List<Integer> factors = new ArrayList<>();
-        if (number>1){
-            int divisor = 2;
-
-            if (number==4||number==6||number==9||number==12)
-            {
-                for(divisor=2; number >1;divisor++) {
-                    while (number % divisor == 0) {
-                        factors.add(divisor);
-                        number /= divisor;
-                    }
-                }
-
-            }
-
-            else {
-
-                factors.add(number);
+        for(int divisor=2; number >1;divisor++) {
+            while (number % divisor == 0) {
+                factors.add(divisor);
+                number /= divisor;
             }
         }
-
         return factors;
     }
 }

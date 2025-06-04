ccec59e457275ad12dbf6988ffedbd264e28c35f
dean <dean4208@gmail.com>
2025-06-04 10:17:24 +0900

    [feature] 29p

--- src/main/java/PrimeFactor.java
+++ src/main/java/PrimeFactor.java
@@ -2,8 +2,12 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class PrimeFactor {
-    public Object of(int i) {
+    public Object of(int number) {
         List<Integer> factors = new ArrayList<>();
+        if (number==2){
+            factors.add(2);
+        }
+
         return factors;
     }
 }
--- src/test/java/PrimeFactorTest.java
+++ src/test/java/PrimeFactorTest.java
@@ -6,8 +6,14 @@ import static org.junit.jupiter.api.Assertions.*;
 class PrimeFactorTest {
 
     @Test
-    void testPrimefactor0f1() {
+    void testPrimefactorOf1() {
         PrimeFactor primeFactor = new PrimeFactor();
         assertEquals(Arrays.asList(),primeFactor.of(1));
+    }
+
+    @Test
+    void testPrimefactorOf2() {
+        PrimeFactor primeFactor = new PrimeFactor();
+        assertEquals(Arrays.asList(2),primeFactor.of(2));
     }
 }

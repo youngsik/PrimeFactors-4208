b97da97af03d7afecdd2ec9d126ae0a67e21de7f
dean <dean4208@gmail.com>
2025-06-04 10:19:35 +0900

    [refactoring] 32p

--- src/test/java/PrimeFactorTest.java
+++ src/test/java/PrimeFactorTest.java
@@ -1,19 +1,23 @@
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 import java.util.Arrays;
 import static org.junit.jupiter.api.Assertions.*;
 
 class PrimeFactorTest {
+    PrimeFactor primeFactor;
+    @BeforeEach
+    void setUp() {
+        primeFactor = new PrimeFactor();
+    }
 
     @Test
     void testPrimefactorOf1() {
-        PrimeFactor primeFactor = new PrimeFactor();
         assertEquals(Arrays.asList(),primeFactor.of(1));
     }
 
     @Test
     void testPrimefactorOf2() {
-        PrimeFactor primeFactor = new PrimeFactor();
         assertEquals(Arrays.asList(2),primeFactor.of(2));
     }
 }
